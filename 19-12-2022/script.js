7b748459595206c395c8bfc29f8795b51e73b00e
FlaviaPinello <121036884+FlaviaPinello@users.noreply.github.com>
2022-12-19 17:36:06 +0100

    esercizio del 19-12-2022

---
+++ 19-12-2022/script.js
@@ -0,0 +1,4 @@
+let userDom = prompt("Quanto fa 3 + 5 ?");
+let userNum = prompt("Inserisci un primo numero");
+let userSecNum = prompt("Inserisci un secondo numero");
+console.log("Hai scritto" + userDom  + userNum +  userSecNum );
